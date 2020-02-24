5ad29e25207be2c988c7c5dc35565b21ef090fb5
Volodymyr Hubernatorov <utmworck@gmail.com>
2020-02-24 11:02:52 +0200

    AutoTesting

--- src/test/java/CheckEmailHappyPath.java
+++ src/test/java/CheckEmailHappyPath.java
@@ -1,5 +1,3 @@
-
-import com.lipsum.Email;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -8,14 +6,12 @@ public class CheckEmailHappyPath {
 
     @DataProvider(name = "ListOfEmails")
     public static Object[] credentials() {
-        return new Object[] { "poor@gmail.com", "po1234or@gm1al.com", "Po12or3@Mail2.com", "sE1@Qa.as",
+        return new Object[]{"poor@gmail.com", "po1234or@gm1al.com", "Po12or3@Mail2.com", "sE1@Qa.as",
                 "asdQwsD!edks534dfcxa@Qds3a.cfgsa"};
     }
 
     @Test(dataProvider = "ListOfEmails")
-    public void checkEmails(String emails){
+    public void checkEmails(String emails) {
         Assert.assertTrue(Email.isEmailCorrect(emails));
     }
-
-
 }
