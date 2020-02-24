6e27db2f405ddd9d6057e2463316f1b3701d7e6e
Volodymyr Hubernatorov <utmworck@gmail.com>
2020-02-24 11:16:00 +0200

    AutoTesting

---
+++ src/test/java/CheckEmailUnhappyPath.java
@@ -0,0 +1,16 @@
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class CheckEmailUnhappyPath {
+
+    @DataProvider(name = "ListOfEmails")
+    public static Object[] credentials() {
+        return new Object[]{"P@g.c", "pozdczcdzaDSA12!1234or@zdzFR1g.comsFffds", "@ . ", "@", ""};
+    }
+
+    @Test(dataProvider = "ListOfEmails")
+    public void testEmailArray(String emails) {
+        Assert.assertFalse(Email.isEmailCorrect(emails));
+    }
+}
