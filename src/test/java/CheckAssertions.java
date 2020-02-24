3725d4d30c503f92abb4a03dd10608525772f8aa
Volodymyr Hubernatorov <utmworck@gmail.com>
2020-02-24 11:18:20 +0200

    AutoTesting

---
+++ src/test/java/CheckAssertions.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckAssertions {
+
+    @Test
+    public void openYouTube() {
+        ChromeDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/Users/Lexx/chromedriv/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://youtube.com/");
+        Assert.assertEquals("YouTube", driver.getTitle());
+    }
+}
